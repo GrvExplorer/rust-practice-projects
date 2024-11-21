c6f1e41ddff8beb79b2cb35cd4a24b7f7c95af32
GrvExplorer <icodelife307@gmail.com>
2024-11-21 23:57:10 +0530

    concatenate

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
 use rand::Rng;
+use colored::Colorize;
+use std::cmp::Ordering;
 
 // *** using Macro ( ! ) ***
 
@@ -16,24 +18,98 @@ use rand::Rng;
 
 fn main() {
     guess_number();
+    // concatenate();
+    // which_marvel_character();
 }
+
 
 // fn longest<'a, >(a: &'a str, b: &'a str) -> &'a str {
 //     if a.len() > b.len() {
 //         a
-//     }else {
+//     }else {a
 //         b
 //     }
 // }
 
 fn guess_number() {
     println!("Guess the number");
-
-    println!("Input your number: ");
-        
+    let secret_number = rand::thread_rng().gen_range(1..40);
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("failed to read read_line");
-    
-    println!("Your guessed {}", guess);
+    loop {
+
+        println!("Input your number: ");
+        io::stdin().read_line(&mut guess).expect("failed to read read_line");
+        
+        let guess = guess.trim().parse::<u32>().expect("Please enter a number");
+        
+        // println!("Your guessed {}", guess);
+
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed it!");
+                break;
+            }
+        }
+        
+    }
+
+    println!("You guessed the number {}", secret_number)
+        
+
 }
+
+
+fn concatenate() {
+
+    println!("Welcome to concatenating your words !");
+
+    
+    let mut name = String::new();
+    println!("What is your name? ");
+    io::stdin().read_line(&mut name).expect("failed to read read_line");
+
+    let mut age = String::new();
+    println!("What is your age? ");
+    io::stdin().read_line(&mut age).expect("failed to read read_line");
+
+    println!("your name is {} and your age is {}", name.purple(), age.green());
+
+}
+
+fn which_marvel_character() {
+    let mut heading = String::from("Which Marvel Character creator you are play");
+    println!("{}", heading.green());
+    println!("-----");
+    println!("Use yes or no as your answer");
+    println!("");
+    let mut answer = String::new();
+
+    println!("Do you like hanging around ? ");
+    io::stdin().read_line(&mut answer).expect("failed to read read_line");
+
+    if answer.to_lowercase().trim() == "yes" {
+        println!("Then you are Spider-Man !")
+    }else {
+        println!("Do you have a gravelly voice ? ");
+        io::stdin().read_line(&mut answer).expect("failed to read read_line");
+        if answer.to_lowercase().trim() == "yes" {
+            println!("Aww, then you're korg !")
+        }else {
+            println!("Do you often feel 'Marvelous' ?");
+            io::stdin().read_line(&mut answer).expect("failed to read read_line");
+            if answer.to_lowercase().trim() == "yes" {
+                println!("You are a captain marvel !");
+            }else {
+                println!("Stop watching Marvel movies from write now OK!\nBetter to Coding");
+            }
+        }
+    }
+
+    println!("Thank you for playing !");
+
+}
+
